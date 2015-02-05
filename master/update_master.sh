3841797ec104d7b8be6b3a5e79b40baf3ac58073
Hans Then <hans.then@gmail.com>
2015-02-05 16:51:38 +0100

    Update master now sets ethernet cards right.

---
+++ master/update_master.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "updating master from source code repository"
+HERE="`( cd \"$(dirname "$0")" && pwd )`"
+cp -Lrv $HERE/rootimg/* /
+
+source /etc/profile.d/xcat.sh
+mkdir -p /tmp/xcattables
+read ETH1 ETH2 ETH3 <<<$(ls /sys/class/net/ | grep "^e" | sort | head -3)
+for table in $(ls $HERE/tables); do
+    sed -e "s/\<eno1\>/$ETH1/g" -e "s/\<eno2\>/$ETH2/g"  -e "s/\<eno3\>/$ETH3/g" $HERE/tables/$table > /tmp/xcattables/$table;
+done
+
+restorexCATdb -Vp /tmp/xcattables  
