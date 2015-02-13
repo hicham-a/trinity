ee1d6cfe06ce931b82425dbb5a43fb483868aeaa
Hans Then <hans.then@gmail.com>
2015-02-13 19:15:36 +0100

    Added an argument to update_master --tables that will also update tables.

--- master/update_master.sh
+++ master/update_master.sh
@@ -7,7 +7,9 @@ cp -Lrv $HERE/rootimg/* /
 (git branch | grep "*"; git rev-parse --short HEAD) > /trinity/version
 cp /trinity/version /trinity/controller/rootimg/install/postscripts/cv_trinity_version
 
-exit 1
+if [ -z $0 ]; then
+    exit 1
+fi
 
 source /etc/profile.d/xcat.sh
 mkdir -p /tmp/xcattables
