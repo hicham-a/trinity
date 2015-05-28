ef1019e64702a9175244958e2a09df10b3919aa4
Hans Then <hans.then@gmail.com>
2015-05-28 09:50:05 +0200

    Updated to openstack juno.

--- master/create_repo_snapshot.sh
+++ master/create_repo_snapshot.sh
@@ -6,7 +6,7 @@ if [ -d /install/post/otherpkgs/centos7.0/x86_64/ ]; then
 	cp -rl /install/post/otherpkgs/centos7.0/x86_64/* /install/post/$DATE/centos7.0/x86_64
         rm /install/post/otherpkgs
 fi
-reposync -n -r epel -r extras -r updates -r xcat-2-core -r xcat-dep -r openstack-icehouse -p /install/post/$DATE/centos7.0/x86_64/
+reposync -n -r epel -r extras -r updates -r xcat-2-core -r xcat-dep -r openstack-juno -p /install/post/$DATE/centos7.0/x86_64/
 ln -sT /install/post/$DATE /install/post/otherpkgs
 createrepo /install/post/otherpkgs/centos7.0/x86_64/
 
