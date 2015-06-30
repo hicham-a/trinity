5ba272c3d8da3b564f92e9f7c708a629df6d5928
Abhishek Mukherjee <abhi.mukh@gmail.com>
2015-06-30 10:05:24 +0200

    Moved to .modulespath to top level, changed resolv.conf back to a HERE doc, using numeric IP for controller

--- login/setup.sh
+++ login/setup.sh
@@ -37,11 +37,11 @@ cp --dereference --recursive --verbose --preserve /trinity/login/rootimg/* /
 #---------------------------------------------------------------------------
 # Hostname resolution
 #---------------------------------------------------------------------------
-##cp-rootimg
-##cat << EOF > /etc/resolv.conf
-##search cluster. vc-a. cluster
-##nameserver 10.141.255.254
-##EOF
+##For now using a HERE document
+cat << EOF > /etc/resolv.conf
+search cluster. vc-a. cluster
+nameserver 10.141.255.254
+EOF
 
 read ETH1 <<<$(ls /sys/class/net/ | grep "^e" | sort | head -1)
 sed -e 's/^PEERDNS="yes"/PEERDNS="no"/g' \
