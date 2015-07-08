59c34ef4abc616f59e4feae00fa8c009521072a7
Hans Then <hans.then@gmail.com>
2015-07-08 13:40:06 +0200

    First stab ar renaming centos7.0 to centos7.

--- master/create_repo_snapshot.sh
+++ master/create_repo_snapshot.sh
@@ -9,17 +9,17 @@ if ! grep -Fq $OSTACKRELEASE /etc/yum.repos.d/rdo-release.repo; then
         exit 1
 fi
 set -x
-mkdir -p /install/post/$DATE/centos7.0/x86_64
-if [ -d /install/post/otherpkgs/centos7.0/x86_64/ ]; then
+mkdir -p /install/post/$DATE/centos7/x86_64
+if [ -d /install/post/otherpkgs/centos7/x86_64/ ]; then
         # Make a copy of the current repo, to avoid that the reposync command that follows 
         #  downloads again stuff that was already in the old repo 
-	cp -rl /install/post/otherpkgs/centos7.0/x86_64/* /install/post/$DATE/centos7.0/x86_64
+	cp -rl /install/post/otherpkgs/centos7/x86_64/* /install/post/$DATE/centos7/x86_64
         # Remove the symlink
         rm /install/post/otherpkgs
 fi
-reposync -n -r epel -r extras -r updates -r xcat-2-core -r xcat-dep -r $OSTACKRELEASE -p /install/post/$DATE/centos7.0/x86_64/
+reposync -n -r epel -r extras -r updates -r xcat-2-core -r xcat-dep -r $OSTACKRELEASE -p /install/post/$DATE/centos7/x86_64/
 # Sync the repo that is in the $DATE directory, downloading only what is missing (faster)
 ln -sT /install/post/$DATE /install/post/otherpkgs
-createrepo /install/post/otherpkgs/centos7.0/x86_64/
+createrepo /install/post/otherpkgs/centos7/x86_64/
 
 
