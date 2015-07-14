c2bcd2977eace705c54041718dd901c502a26169
Abhishek Mukherjee <abhi.mukh@gmail.com>
2015-07-14 10:19:32 +0200

    Init file used only if database is not initialized

--- openstack/dockerize/osdb/docker-entrypoint.sh
+++ openstack/dockerize/osdb/docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-chown -R mysql:mysql /var/lib/mysql
-
+if [ ! -d /var/lib/mysql/mysql ]; then  
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
+  chown -R mysql:mysql /var/lib/mysql
+  cat > /etc/my.cnf.d/dbinit.cnf <<-EOF
+	[mysqld]
+	init-file=/tmp/dbinit.sql
+	EOF
+fi
 exec "$@"
