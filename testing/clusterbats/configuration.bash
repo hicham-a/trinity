89e366358dde8e03dc412c486ada45e17d0e3009
Hans Then <hans.then@gmail.com>
2016-02-02 07:56:31 +0000

    Moved the clusterbats back into the separate testing repository.

--- testing/clusterbats/configuration.bash
+++
@@ -1,8 +0,0 @@
-CONFIG=/trinity/testing/clusterbats/$(</trinity/site).cfg
-source ${CONFIG}
-CONTAINERS=${NODES/node/c}
-
-expand() {
-    lsdef "$@" | grep "Object name" | awk -F': ' '{print $2}'
-}
-
--- testing/install_bats.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yum -y -q install git
-git clone https://github.com/sstephenson/bats.git && bats/install.sh /usr/local
