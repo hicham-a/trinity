3b84b342592d1fd48769fb2359dad2217dd01751
Hicham Amrati <hicham.amrati@clustervision.com>
2016-04-04 13:18:13 +0200

    Fixes #665

--- container/rootimg/docker-entrypoint.sh
+++ container/rootimg/docker-entrypoint.sh
@@ -1,4 +1,9 @@
-for f in /docker-entrypoint-startup.d/*; do
-    echo "$0: running $f"; . "$f"
-done
+#!/bin/bash
+
+if [[ -d /docker-entrypoint-startup.d ]]; then
+    for f in /docker-entrypoint-startup.d/*; do
+        echo "$0: running $f"; . "$f"
+    done
+fi
+
 exec "$@"
