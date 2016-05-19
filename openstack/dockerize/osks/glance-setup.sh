51cfbba2bea9978aa8cd40c1e0cb6a9347af84c6
Hicham Amrati <hicham.amrati@clustervision.com>
2016-05-19 13:28:12 +0200

    Delete obsolete openstack config files

--- openstack/dockerize/osks/glance-setup.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-IP=$(hostname -i)
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 user-create --name glance --pass system 
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 user-role-add --user glance --tenant service --role admin
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 service-create --name glance --type image --description "OpenStack Image Service"
-SERVICE_ID=$(docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 service-list | awk '/ image / {print $2}')
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 endpoint-create --service-id "${SERVICE_ID}" --publicurl http://${IP}:9292 --internalurl http://${IP}:9292 --adminurl http://${IP}:9292 --region regionOne
--- openstack/dockerize/osks/nova-setup.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-IP=$(hostname -i)
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 user-create --name nova --pass system
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 user-role-add --user nova --tenant service --role admin
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 service-create --name nova --type compute --description "OpenStack Compute"
-SERVICE_ID=$(docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 service-list | awk '/ compute / {print $2}')
-docker exec osks keystone --os-token system --os-endpoint http://${IP}:35357/v2.0 endpoint-create --service-id "${SERVICE_ID}" --publicurl http://${IP}:8774/v2/%\(tenant_id\)s --internalurl http://${IP}:8774/v2/%\(tenant_id\)s --adminurl http://${IP}:8774/v2/%\(tenant_id\)s --region regionOne
-
